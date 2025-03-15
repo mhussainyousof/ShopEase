8f7db5cd41474a5141cfb65d65ad7e848651f738
Hussain Yousof <mhussainyousof@gmail.com>
2025-03-15 13:15:10 +0330

    Custom Appbar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:shop_ease/common/widgets/appbar/appbar.dart';
 import 'package:shop_ease/common/widgets/custom_shapes/containers/demo_curved_widget.dart';
 import 'package:shop_ease/common/widgets/products/cart/bag_count.dart';
 import 'package:shop_ease/utils/constants/colors.dart';
+import 'package:shop_ease/utils/constants/sizes.dart';
 import 'package:shop_ease/utils/constants/text_strings.dart';
+import 'package:shop_ease/utils/device/device_utility.dart';
+import 'package:shop_ease/utils/helpers/helper_functions.dart';
 
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +42,57 @@ class HomeScreen extends StatelessWidget {
                       BagCountWidget(
                         iconColor: TColors.white,
                         onPressed: () {},
-                      )
+                      ),
                     ],
                   ),
+                  SizedBox(height: TSizes.spaceBtwSections ,),
+                  SearchContainer(
+                    showBorder: false,
+                    text: 'Search here...',
+                    
+
+                  )
+
                 ],
               ),
-            )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class SearchContainer extends StatelessWidget {
+  const SearchContainer({
+    super.key, this.icon = Iconsax.search_normal,  required this.text,  this.showBackground = true,  this.showBorder = true,
+  });
+
+  final IconData? icon;
+  final String text;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: TSizes.defaultSpace - 2),
+      child: Container(
+        width: TDeviceUtils.getScreenWidth(context),
+        padding: EdgeInsets.all(TSizes.sm + 5),
+        decoration: BoxDecoration(
+          color: showBackground ? dark ? TColors.dark : TColors.light : Colors.transparent,
+          borderRadius: BorderRadius.circular(TSizes.borderRadiusLg),
+          border: showBorder ? Border.all(color: TColors.grey) : null
+      
+          
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: TColors.darkGrey,),
+            SizedBox(width: TSizes.spaceBtwItems,),
+            Text(text, style:Theme.of(context).textTheme.bodySmall)
           ],
         ),
       ),
