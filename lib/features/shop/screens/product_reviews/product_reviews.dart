a3c5b55af571b3eb5fa7e2fd3342f69e6af40320
Hussain Yousof <mhussainyousof@gmail.com>
2025-03-28 23:02:14 +0330

    Flutter Rating Bar Package

--- lib/features/shop/screens/product_reviews/product_reviews.dart
+++ lib/features/shop/screens/product_reviews/product_reviews.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:shop_ease/common/widgets/appbar/appbar.dart';
+import 'package:shop_ease/utils/constants/colors.dart';
 import 'package:shop_ease/utils/constants/sizes.dart';
+import 'package:shop_ease/utils/device/device_utility.dart';
 
 class ProductReviewsScreen extends StatelessWidget {
   const ProductReviewsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final theme = Theme.of(context).textTheme;
    return Scaffold(
     appBar: MyAppBar(
       showBackArrow: true,
@@ -24,8 +26,36 @@ class ProductReviewsScreen extends StatelessWidget {
           SizedBox(height: TSizes.spaceBtwItems),
 
           Row(children: [
-            Text('4.8', style: theme.textTheme.displayLarge,),
-            
+            Expanded(
+              flex: 3,
+              child: Text('4.8', style: theme.displayLarge!.apply(fontSizeFactor: 0.65),)),
+            Expanded(
+              flex: 7,
+              child: Column(children: [
+                Row(
+                  children: [
+              
+                    Expanded(
+                      flex: 1,
+                      child: Text('5', style: theme.bodyMedium)),
+                    Expanded(
+                      flex: 11,
+                      child: SizedBox(
+                        width: TDeviceUtils.getScreenWidth(context) * 0.5,
+                        child: LinearProgressIndicator(
+                          value: 0.5,
+                          minHeight: 11,
+                          backgroundColor: TColors.grey,
+                          borderRadius: BorderRadius.circular(7),
+                          valueColor: AlwaysStoppedAnimation(TColors.primary),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],),
+            )
+
           ],)
 
         ],
