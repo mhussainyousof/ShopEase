4ec2b84eda355b7142e3e0552eefcd78e37594b3
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-06 20:37:04 +0330

    ...

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_ease/common/widgets/success_screen/success_screen.dart';
+import 'package:shop_ease/features/authentication/controllers/signup/verify_email_controller.dart';
 import 'package:shop_ease/features/authentication/screens/login/login.dart';
 import 'package:shop_ease/utils/constants/image_strings.dart';
 import 'package:shop_ease/utils/constants/sizes.dart';
@@ -16,7 +17,7 @@ class VerifyEmailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(VerifyEmailScreen());
+    final controller = Get.put(VerifyEmailController());
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -41,12 +42,17 @@ class VerifyEmailScreen extends StatelessWidget {
 
           SizedBox(height: TSizes.spaceBtwSections,),
 
-          SizedBox(width: double.infinity, child: ElevatedButton(onPressed: ()=> Get.to(()=> SuccessScreen(
-            image: TImages.staticSuccessIllustration,
-            title: TTexts.yourAccountCreatedTitle,
-            subTitle: TTexts.yourAccountCreatedSubTitle,
-            onPressed:()=> Get.to(()=> LoginScreen()),
-          )), child: Text(TTexts.tContinue)),),
+          SizedBox(width: double.infinity, child: ElevatedButton(onPressed: ()=> 
+          controller.checkEmailVerificationStatus(),
+          // Get.to(()=> SuccessScreen(
+          //   image: TImages.staticSuccessIllustration,
+          //   title: TTexts.yourAccountCreatedTitle,
+          //   subTitle: TTexts.yourAccountCreatedSubTitle,
+          //   onPressed:()=> Get.to(()=> LoginScreen()),
+          // )),
+          child: Text(TTexts.tContinue)
+          ),
+          ),
 
           SizedBox(height: TSizes.spaceBtwItems,),
 
