b8b125a317da1cd7e122fe53fd3a283ac0fe7c84
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-12 17:35:53 +0330

    Google SignIn is Started

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shop_ease/features/authentication/screens/login/login.dart';
 import 'package:shop_ease/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:shop_ease/features/authentication/screens/signup/verify_email.dart';
@@ -64,6 +65,25 @@ class AuthenticationRepository extends GetxController {
 
 
   Future<UserCredential> registerWithEmailAndPassword(String email, String password)async{
+    try{
+    final GoogleSignInAccount? userAccount = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth = await userAccount?.authentication;
+    final credentials = GoogleAuthProvider.credential();
+
+    }on FirebaseAuthException catch(e){
+      throw TFirebaseAuthException(e.code).message;
+    }on FirebaseException catch(e){
+      throw TFirebaseException(e.code);
+    }on FormatException catch(_){
+      throw TFormatException();
+    }on PlatformException catch(e){
+      throw TPlatformException(e.code).message;
+    }catch(e){
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  Future<UserCredential> signInWithGoogle(String email, String password)async{
     try{
       return await _auth.createUserWithEmailAndPassword(email: email, password: password);
 
