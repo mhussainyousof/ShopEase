0142a0b90953021dd9c89083505dd48b6bdfc31d
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-19 08:48:11 +0330

    ..

--- lib/features/shop/controllers/mock_categories.dart
+++ lib/features/shop/controllers/mock_categories.dart
@@ -1,59 +1,60 @@
 
 import 'package:shop_ease/features/shop/models/categories_model.dart';
+import 'package:shop_ease/utils/constants/image_strings.dart';
 
 final mockCategories = [
   CategoryModel(
     id: '1',
     name: 'Sport',
-    image: 'assets/icons/categories/icons8-bowling-64.png',
+    image: TImages.sportIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '2',
     name: 'Clothing',
-    image: 'assets/icons/categories/icons8-tailors-dummy-64.png',
+    image: TImages.clothIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '3',
     name: 'Shoes',
-    image: 'assets/icons/categories/icons8-shoes-64.png',
+    image: TImages.shoeIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '4',
     name: 'Cosmetics',
-    image: 'assets/icons/categories/icons8-cosmetics-64.png',
+    image: TImages.cosmeticsIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '5',
     name: 'Animals',
-    image: 'assets/icons/categories/icons8-dog-heart-64.png',
+   image: TImages.animalIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '6',
     name: 'Toys',
-    image: 'assets/icons/categories/icons8-wooden-toy-car-50.png',
+    image: TImages.toyIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '7',
     name: 'Furniture',
-    image: 'assets/icons/categories/icons8-dining-chair-64.png',
+    image: TImages.furnitureIcon,
     isFeatured: false,
   ),
   CategoryModel(
     id: '8',
     name: 'Jewelry',
-    image: 'assets/icons/categories/icons8-sparkling-diamond-64.png',
+    image: TImages.jeweleryIcon,
     isFeatured: true,
   ),
   CategoryModel(
     id: '9',
     name: 'Electronics',
-    image: 'assets/icons/categories/icons8-smartphone-64.png',
+    image: TImages.electronicsIcon,
     isFeatured: true,
   ),
 ];
