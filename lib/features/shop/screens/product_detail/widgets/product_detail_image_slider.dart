d9015bd721d3eb90433bcfdfcb182aed740bf7c1
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-21 22:13:26 +0330

    Get Done with Product Image

--- lib/features/shop/screens/product_detail/widgets/product_detail_image_slider.dart
+++ lib/features/shop/screens/product_detail/widgets/product_detail_image_slider.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shop_ease/common/widgets/appbar/appbar.dart';
 import 'package:shop_ease/common/widgets/custom_shapes/curved_edges/main_curve.dart';
@@ -33,47 +34,70 @@ class EProductImageSlider extends StatelessWidget {
       child: Stack(
         children: [
           SizedBox(
-            height: 320,
-            child: Padding( 
-              padding: const EdgeInsets.all(TSizes.productImageRadius * 2),
-              child: Center(child: 
-              Obx((){
-                final image = controller.selectedProductImage.value;
-                return CachedNetworkImage(imageUrl: image);
-              })
-              ),
-            )),
+              height: 320,
+              child: Padding(
+                padding: const EdgeInsets.all(TSizes.productImageRadius * 2),
+                child: Center(
+                  child: Obx(() {
+                  final image = controller.selectedProductImage.value;
+                  return GestureDetector(
+                    onTap: ()=> controller.showEnlargedImage(image),
+                    child: CachedNetworkImage(
+                        imageUrl: image,
+                        progressIndicatorBuilder: (_, __, downloadProgress) =>
+                            CircularProgressIndicator(
+                              value: downloadProgress.progress,
+                              color: TColors.primary,
+                            )),
+                  );
+                })),
+              )),
+          Positioned(
+            right: 0,
+            bottom: 30,
+            left: TSizes.defaultSpace,
+            child: SizedBox(
+              height: 70,
+              child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (_, index) {
+                    return Obx(() {
+                      final imageSelected =
+                          controller.selectedProductImage.value ==
+                              images[index];
 
-         Positioned(
-          right: 0,
-          bottom: 30,
-          left: TSizes.defaultSpace,
-           child: SizedBox(
-            height: 70,
-             child: ListView.separated(
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              physics: AlwaysScrollableScrollPhysics(),
-              itemBuilder: (_,index){
-              return  ERoundedImage(
-                width: 70,
-                backgroundColor: dark ? TColors.dark : TColors.white,
-                border: Border.all(color: TColors.primary),
-                padding: EdgeInsets.all(TSizes.sm),
-                imageUrl: TImages.productImage3);
-             }, separatorBuilder: (_,__){
-                                   return  SizedBox(width: TSizes.spaceBtwItems);
-             }, itemCount: 6),
-           ),
-         ),
-    
-         MyAppBar(
-          showBackArrow: true,
-          actions: [
-            ECircularIcon(icon: Iconsax.heart5, color: Colors.red,)
-          ],
-         )
-            
+                      return ERoundedImage(
+                        onTap: () => controller.selectedProductImage.value =
+                        images[index],
+                        width: 70,
+                        isNetworkImage: true,
+                        imageUrl: images[index],
+                        padding: EdgeInsets.all(TSizes.sm),
+                        backgroundColor: dark ? TColors.dark : TColors.white,
+                        border: Border.all(
+                            color: imageSelected
+                                ? TColors.primary
+                                : Colors.transparent),
+                      );
+                    });
+                  },
+                  separatorBuilder: (_, __) {
+                    return SizedBox(width: TSizes.spaceBtwItems);
+                  },
+                  itemCount: images.length),
+            ),
+          ),
+          MyAppBar(
+            showBackArrow: true,
+            actions: [
+              ECircularIcon(
+                icon: Iconsax.heart5,
+                color: Colors.red,
+              )
+            ],
+          )
         ],
       ),
     ));
