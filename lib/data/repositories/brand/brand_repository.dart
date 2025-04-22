cf519de5f46887bbcccc0ad50c2dc6c467bdd172
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-22 16:36:14 +0330

    Brands Backend has bees started

---
+++ lib/data/repositories/brand/brand_repository.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:shop_ease/features/shop/models/brand_model.dart';
+import 'package:shop_ease/utils/exceptions/firebase_exceptions.dart';
+import 'package:shop_ease/utils/exceptions/format_exceptions.dart';
+import 'package:shop_ease/utils/exceptions/platform_exceptions.dart';
+
+class BrandRepository extends GetxController {
+  static BrandRepository get instance => Get.find();
+
+  // Variables
+  final _db = FirebaseFirestore.instance;
+
+  // Get all brands
+  Future<List<BrandModel>> getAllBrands() async {
+    try {
+      final snapshot = await _db.collection('Brands').get();
+      final result = snapshot.docs.map((e) => BrandModel.fromSnapshot(e)).toList();
+      return result;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong while fetching Banners.';
+    }
+  }
+
+  // Get Brands For Category
+  Future<List<BrandModel>> getBrandsForCategory(String categoryId) async {
+    try {
+      final snapshot = await _db
+          .collection('Brands')
+          .where('CategoryId', isEqualTo: categoryId)
+          .get();
+      final result = snapshot.docs.map((e) => BrandModel.fromSnapshot(e)).toList();
+      return result;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const TFormatException();
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong while fetching brands for category.';
+    }
+  }
+}
