0b2fcd2b681f2f187858a7adb4312638f70a16ff
Hussain Yousof <mhussainyousof@gmail.com>
2025-04-22 15:36:37 +0330

    Working on all popular products page

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,23 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shop_ease/common/widgets/appbar/appbar.dart';
+import 'package:shop_ease/common/widgets/shimmers/vertical_product_shimmer.dart';
 import 'package:shop_ease/common/widgets/sortable/sortable_products.dart';
+import 'package:shop_ease/features/shop/controllers/all_product_controller.dart';
+import 'package:shop_ease/features/shop/models/product_model.dart';
 import 'package:shop_ease/utils/constants/sizes.dart';
 
 class AllProductsScreen extends StatelessWidget {
-  const AllProductsScreen({super.key});
+  const AllProductsScreen(
+      {super.key, required this.title, this.query, this.futureMethod});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: MyAppBar(title: Text("Popular Products"), showBackArrow: true),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(TSizes.defaultSpace),
-          child: ESortableProducts(),
-          // child: SizedBox(),
+  final String title;
+  final Query? query;
+  final Future<List<ProductModel>>? futureMethod;
+
+ @override
+Widget build(BuildContext context) {
+  // Initialize controller for managing product fetching
+  final controller = Get.put(AllProductsController());
+
+  return Scaffold(
+    // AppBar
+    appBar: MyAppBar(title: Text(title), showBackArrow: true),
+
+    body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: FutureBuilder(
+          future: futureMethod ?? controller.fetchProductsByQuery(query),
+          builder: (context, snapshot) {
+            // Check the state of the FutureBuilder snapshot
+            const loader = EVerticalProductShimmer();
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return loader;
+            }
+
+            if (!snapshot.hasData || snapshot.data == null || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No Data Found!'));
+            }
+
+            if (snapshot.hasError) {
+              return const Center(child: Text('Something went wrong.'));
+            }
+
+            // Products found!
+            final products = snapshot.data!;
+
+            return  ESortableProducts(products: products,);
+          },
         ),
       ),
-    );
-  }
+    ),
+  );
 }
 
+}
